c2cd0adbc8d09d514081cc90e08f3ebf185930c5
Felix Schaede <felix.schaede@softwareag.com>
2023-01-04 12:05:50 +0100

    added logging

--- watcher.py
+++ watcher.py
@@ -75,11 +75,13 @@ if __name__== "__main__":
         client.connect(broker, port)
         logger.info("Subscribe")
         client.subscribe("tedge/health/#")
+        logger.info("Loop")
         client.loop_forever()
         logger.info("Starting")
         # Create a thread that will run the check_container_status function
         thread = threading.Thread(target=check_container_status, args=(client,))
         # Start the thread
+        logger.info("Start docker loop")
         thread.start()
         
             
